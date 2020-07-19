a91b274b7209c899be1f93facd77ab42bf654c9d
Rusiamba <rus.orudjev@mail.ru>
2020-07-19 10:56:45 +0300

    FFX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import './css/style.css';
-
-import './js/app';
-
-// TODO: write your code in app.js
+export default function getHealth(user) {
+    if(user.health >= 15 && user.health < 50){
+      return 'wounded'
+    }
+    else if((user.health >= 50)){
+      return 'healthy'
+    }
+    return 'critical'
+}
--- src/js/app.js
+++
@@ -1,5 +0,0 @@
-// TODO: write your code here
-export default function sortByHealth(arr) {
-    arr.sort((a, b) => (a.health > b.health ? 1 : -1));
-    return arr.reverse();
-}
