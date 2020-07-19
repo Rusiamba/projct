c19c434712020febde7fec7ba60ec8a54cf2232b
Rusiamba <rus.orudjev@mail.ru>
2020-07-19 10:49:04 +0300

    First fix

--- src/js/Game/game.js
+++
@@ -1,8 +0,0 @@
-export default function GameObj() {
-    let push = {
-        name: 'Маг', 
-        health: 90
-    }
-
-    return push;
-}
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 // TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default function sortByHealth(arr) {
+    arr.sort((a, b) => (a.health > b.health ? 1 : -1));
+    return arr.reverse();
+}
